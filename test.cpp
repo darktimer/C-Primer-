bcc33699df631fc4949bdbd33916d02285a95c61
darktimer <darktimer.yz@outlook.com>
2019-12-24 23:22:38 +0800

    Create test.cpp
    test

---
+++ test.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	double a = 1000000000;
+
+	double b = 123.456;
+
+	printf("%e\n", a);
+
+	printf("%f\n", a);
+
+	printf("%lf\n", a);
+
+	printf("%g\n", a);
+
+	printf("%e\n", b);
+
+	printf("%f\n", b);
+
+	printf("%lf\n", b);
+
+	printf("%g\n", b);
+
+	return 0;
+}
