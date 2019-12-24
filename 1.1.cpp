e3422cccc9d71a38f4e80626c20327048a2b34b5
darktimer <darktimer.yz@outlook.com>
2019-12-24 22:44:12 +0800

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+    double inch, centimeter;
+
+    printf("please enter inches:");
+    scanf_s("%lf", &inch);
+
+    centimeter = inch / 2.54;
+
+    printf("%.2lf inches is %g centimeter", inch, centimeter);
+     
+    return 0;
+}
+
